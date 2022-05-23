d1a0705773a6bf73d4c2f0aaeed14524fc5fcd2e
docherak <docherak@protonmail.ch>
2022-05-23 23:55:51 +0200

    Layout done
    Finished HTML layout and CSS styling for now. Moving to JavaScript

--- scripts.js
+++ scripts.js
@@ -1,31 +1,29 @@
 const container = document.querySelector('.container');
-//const div = document.createElement('div')
-//div.classList.add('grid');
-//div.textContent = "";
 
-createGrid();
+const slider = document.getElementById('myRange');
+const output = document.getElementById('cell-pixels');
+output.innerHTML = slider.value;
 
-function createGrid() {
-  for (var i = 0; i < 16; i++) {
-    initiateRow();
+
+createGrid(16);
+
+
+// Functions
+
+function createGrid(size) {
+  for (var i = 0; i < size; i++) {
+    initiateRow(size);
   }
 }
 
-function initiateRow() {
-  for (var i = 0; i < 16; i++) {
+function initiateRow(size) {
+  for (var i = 0; i < size; i++) {
     const div = document.createElement('div');
-    div.textContent = "";
-    container.appendChild(div).className = 'grid';
+    div.setAttribute('id', 'cell');
+    container.appendChild(div);
   }
 }
 
-//let grid = [];
-//for (var i = 0; i < 16; i++) {
-//  grid.push(div);
-//}
-//grid.forEach(elem => container.appendChild(elem));
-
-//for (var i = 0; i < 16; i++) {
-//  container.appendChild(div);
-//  console.log(container);
-//}
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
